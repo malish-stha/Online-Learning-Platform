84460ef1d5bbdede5792d230ddd47f5e609f0192
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-01-24 15:16:17 +0545

    Add error middleware to app.

---
+++ server/middleware/error.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/ErrorHandler";
+export const ErrorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal server error";
+
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  if (err.name === "JsonWebTokenError") {
+    const message = `Invalid token. Please login again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  if (err.name === "TokenExpiredError") {
+    const message = `Token expired. Please login again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
