fb6f3d0eac4d7ab89d4fb22d9d8f342e7ae482ef
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-01-24 19:36:20 +0545

    Add Redis cache for user logout.

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -8,6 +8,7 @@ import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
 import { sendToken } from "../utils/jwt";
+import { redis } from "../utils/redis";
 
 //register user
 interface IRegistrationBody {
@@ -160,6 +161,8 @@ export const logoutUser = CatchAsyncError(
     try {
       res.cookie("access_token", "", { maxAge: 1 });
       res.cookie("refresh_token", "", { maxAge: 1 });
+      const userId = req.user?._id || "";
+      redis.del(userId);
       res.status(200).json({
         success: true,
         message: "User logged out successfully",
