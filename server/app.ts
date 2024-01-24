5b188969a71b866b87c48830ff46665c09d74306
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-01-24 14:03:18 +0545

    Update server configuration and added Database connection.

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,29 @@
-import express from "express";
-
+require("dotenv").config();
+import express, { Request, Response, NextFunction } from "express";
 export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//body parser
+app.use(express.json({ limit: "50mb" }));
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
