954c764728bac7f0be83d79f24375039e5113232
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-01-26 19:40:51 +0545

    Add notifications for new questions and answers.

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -9,6 +9,7 @@ import mongoose from "mongoose";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
+import NotificationModel from "../models/notificationModel";
 
 //upload course
 export const uploadCourse = CatchAsyncError(
@@ -200,6 +201,12 @@ export const addQuestion = CatchAsyncError(
       //add this question to our course content
       courseContent.questions.push(newQuestion);
 
+      await NotificationModel.create({
+        user: req.user?._id,
+        title: "New Question",
+        message: `You have a new question in ${courseContent.title}`,
+      });
+
       //save the updated course
       await course?.save();
 
@@ -261,7 +268,11 @@ export const addAnswer = CatchAsyncError(
       await course?.save();
 
       if (req.user?._id === question.user._id) {
-        //create a notification
+        await NotificationModel.create({
+          user: req.user?._id,
+          title: "New Question Reply Received",
+          message: `You have a new answer in ${courseContent.title}`,
+        });
       } else {
         const data = {
           name: question.user.name,
