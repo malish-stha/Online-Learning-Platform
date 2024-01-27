32d48118cfb6493d98db6d29fc42fc610edc795b
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-01-27 13:35:32 +0545

    Add deleteCourse function for admin.

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -4,6 +4,7 @@ import {
   addQuestion,
   addReplyToReview,
   addReview,
+  deleteCourse,
   editCourse,
   getAllCourses,
   getCourseByUser,
@@ -51,6 +52,13 @@ courseRouter.put(
   isAuthenticated,
   authorizeRoles("admin"),
   getAllCourses
+);
+
+courseRouter.delete(
+  "/delete-course/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  deleteCourse
 );
 
 export default courseRouter;
