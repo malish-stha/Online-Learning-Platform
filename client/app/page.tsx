e9f3279f0b2192a254d13ae531f82beee97dfab2
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-01-28 20:40:33 +0545

    Added different components and made changes to them.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
+import Header from "./components/Header";
 
 interface Props {}
 
 const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
   return (
     <div>
       <Heading
@@ -12,6 +15,7 @@ const Page: FC<Props> = (props) => {
         description="It is a learning platform for students"
         keywords="LMS, MERN, React Nextjs Express"
       />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
     </div>
   );
 };
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,14 +6,15 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: ["class"],
   theme: {
     extend: {
       fontFamily: {
-        Poppins: ["var(--font-Poppins"],
-        Josefin: ["var(--font-Josefin"],
+        Poppins: ["var(--font-Poppins)"],
+        Josefin: ["var(--font-Josefin)"],
       },
       backgroundImage: {
-        "gradient-raidal": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
@@ -30,4 +31,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
