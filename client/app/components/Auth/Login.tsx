e8d7116707f7d783f33b0a4282513ffb6d7dfa25
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-01-30 20:10:27 +0545

    Add social authentication and session provider.

--- client/app/components/Auth/Login.tsx
+++ client/app/components/Auth/Login.tsx
@@ -10,6 +10,7 @@ import { FcGoogle } from "react-icons/fc";
 import { styles } from "../../styles/style";
 import { useLoginMutation } from "@/redux/features/auth/authApi";
 import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Props = {
   setRoute: (route: string) => void;
@@ -113,8 +114,16 @@ const Login: FC<Props> = ({ setRoute, setOpen }) => {
           Or join with
         </h5>
         <div className="flex items-center justify-center my-3">
-          <FcGoogle className="cursor-pointer mr-2" size={30} />
-          <AiFillGithub className="cursor-pointer ml-2" size={30} />
+          <FcGoogle
+            className="cursor-pointer mr-2"
+            size={30}
+            onClick={() => signIn("google")}
+          />
+          <AiFillGithub
+            className="cursor-pointer ml-2"
+            size={30}
+            onClick={() => signIn("github")}
+          />
         </div>
         <h5 className="text-center pt-4 font-Poppins text-[14px]">
           Don't have any account?{" "}
--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Link from "next/link";
 import NavItems from "../utils/NavItems";
 import { ThemeSwitcher } from "../utils/ThemeSwitcher";
@@ -11,6 +11,9 @@ import Verification from "./Auth/Verification";
 import { useSelector } from "react-redux";
 import Image from "next/image";
 import avatar from "../../public/assets/banner.png";
+import { useSession } from "next-auth/react";
+import { useSocialAuthMutation } from "@/redux/features/auth/authApi";
+import toast from "react-hot-toast";
 
 type Props = {
   open: boolean;
@@ -24,6 +27,23 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
   const [active, setActive] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
   const { user } = useSelector((state: any) => state.auth);
+  const { data } = useSession();
+  const [socialAuth, { isSuccess, error }] = useSocialAuthMutation();
+
+  useEffect(() => {
+    if (!user) {
+      if (data) {
+        socialAuth({
+          email: data?.user?.email,
+          name: data?.user?.name,
+          avatar: data?.user?.image,
+        });
+      }
+    }
+    if (isSuccess) {
+      toast.success("Logged in Successfully");
+    }
+  }, [data, user]);
 
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", () => {
