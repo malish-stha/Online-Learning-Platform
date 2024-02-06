545cb64559a8ac881ec3de02abf0dcd1dfeed55e
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-02-06 21:37:16 +0545

    Refactor Profile component styling

--- client/app/components/Profile/Profile.tsx
+++ client/app/components/Profile/Profile.tsx
@@ -15,15 +15,15 @@ const Profile: FC<Props> = (props: Props) => {
       }
     });
   }
-  return (
-    <div className="w-[85%] flex mx-auto">
-      <div
-        className={`w-[60px] 800px:w-[310px] h-[450px] bg-slate-900 bg-opacity-90 border border-[#ffffff1d] rounded-[5px] shadow-sm mt-[80px] mb-[80px] sticky ${
-          scroll ? "top-[120px]" : "top-[30px]"
-        } left-[30px]`}
-      ></div>
-    </div>
-  );
-};
+//   return (
+//     <div className="w-[85%] flex mx-auto">
+//       <div
+//         className={`w-[60px] 800px:w-[310px] h-[450px] bg-slate-900 bg-opacity-90 border border-[#ffffff1d] rounded-[5px] shadow-sm mt-[80px] mb-[80px] sticky ${
+//           scroll ? "top-[120px]" : "top-[30px]"
+//         } left-[30px]`}
+//       ></div>
+//     </div>
+//   );
+// };
 
-export default Profile;
+// export default Profile;
