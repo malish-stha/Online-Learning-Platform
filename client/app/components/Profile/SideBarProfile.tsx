1efb64f2e38305d8ac0b347c7a132e8f774e2c09
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-02-08 20:35:47 +0545

    Update avatar image and add account name to SideBarProfile.

--- client/app/components/Profile/SideBarProfile.tsx
+++ client/app/components/Profile/SideBarProfile.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React, { FC } from "react";
-import avatarDefault from "../../../public/assets/client.jpg";
+import avatarDefault from "../../../public/assets/banner.png";
 
 type Props = {
   user: any;
@@ -21,7 +21,7 @@ const SideBarProfile: FC<Props> = ({
     <div className="w-full">
       <div
         className={`w-full flex items-center px-3 py-4 cursor-pointer ${
-          active === 1 ? "bg-slate-800" : "bg-transparent"
+          active === 1 ? "dark:bg-slate-800 bg-white" : "bg-transparent"
         }`}
         onClick={() => setActive(1)}
       >
@@ -30,6 +30,9 @@ const SideBarProfile: FC<Props> = ({
           alt="img"
           className="w-[20px] h-[20px] 800px:w-[30px] 800px:h-[30px] cursor-pointer rounded-full"
         />
+        <h5 className="pl-2 800px:block hidden font-Poppins dark:text-white text-black">
+          My Account
+        </h5>
       </div>
     </div>
   );
