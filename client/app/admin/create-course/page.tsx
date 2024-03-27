f00c7e8c660280ecdfc26ea5974700b3e39ca5c0
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-03-26 17:23:35 -0700

    Add sidebar, heading, and dashboard components to create-course page.

--- client/app/admin/create-course/page.tsx
+++ client/app/admin/create-course/page.tsx
@@ -1,9 +1,31 @@
+"use client";
 import React from "react";
+import Sidebar from "@/app/components/Admin/Sidebar/AdminSidebar";
+import Heading from "@/app/utils/Heading";
+import CreateCourse from "@/app/components/Admin/Course/CreateCourse";
+import DashboardHeader from "@/app/components/Admin/DashboardHeader";
 
 type Props = {};
 
 const page = (props: Props) => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Heading
+        title="LMS"
+        description="LMS is a platform for students  to learn"
+        keywords="LMS, learn, students"
+      />
+      <div className="flex">
+        <div className="1500px:w-[16%] w-1/5">
+          <Sidebar />
+        </div>
+        <div className="w-[85%]">
+          <DashboardHeader />
+          <CreateCourse />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
---
+++ client/app/components/Admin/Course/CourseInformation.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const CourseInformation = (props: Props) => {
+  return <div>CourseInformation</div>;
+};
+
+export default CourseInformation;
